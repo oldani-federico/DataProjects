89f6b79152399341f3f0d43516b8a796088cc324
Federico Oldani <54869637+oldani-federico@users.noreply.github.com>
2020-07-30 18:40:12 -0400

    Update and rename TheGasProject.R to CleaningGasDataUsingR/TheGasProject.R

--- CleaningGasDataUsingR/TheGasProject.R
+++ CleaningGasDataUsingR/TheGasProject.R
@@ -1,15 +1,28 @@
+# Loading necessary libraries:
 library(tidyverse)
 library(stringr)
-library(TSA)
+
+# Reading in chat extracted from Whatsapp with Gas entries:
 chat = read.csv("C:/Users/fede2/OneDrive - University of Miami/MSBA/WhatsApp Chat with +1 (954) 937-4260.txt")%>%
+
+# Starting the cleaning process using dplyr
+# Renaming columns:
   rename(Date = X12.5.16,
          Entry = X20.03...Messages.to.this.chat.and.calls.are.now.secured.with.end.to.end.encryption..Tap.for.more.info.)%>%
+
+# Filtering, looking only for texts that have the Gas entries:
   filter(str_detect(Entry, 'miles'))%>%
+
+# Resolving spacing issues within the entries:
   mutate(Entry = gsub('miles/', ' miles /', Entry))%>%
   separate(col = Entry, into = c('old','Step1'), sep = 'Oldani: ', remove = FALSE)%>%
   separate(col = Step1, into = c('Miles', 'Step2'), sep = 'miles / \\$')%>%
   mutate(Miles = gsub(' ', '', Miles))%>%
+
+# Creating new columns for Cost and Gallons of Gas:
   separate(col = Step2, into = c('Cost', 'Gallons'), sep = ' per ')%>%
+
+# Calculating a few insightful metrics off of the Gas entries:
   mutate(Miles = as.numeric(Miles),
          Date = as.Date(Date, format = '%m/%d/%y'),
          Cost = as.numeric(gsub(' ', '', Cost)),
@@ -18,6 +31,6 @@ chat = read.csv("C:/Users/fede2/OneDrive - University of Miami/MSBA/WhatsApp Cha
          CostperGallon = as.numeric(round(Cost / Gallons, 2)),
          MilesperGallon = as.numeric(round(c(0, Distance[2:length(Distance)] / Gallons[1:(length(Distance) - 1)]), 2)))%>%
   select(-Entry,-old)%>%
-  write.csv("C:/Users/fede2/OneDrive - University of Miami/MSBA/GasTrackingCLEAN.csv")
-d = read.csv("C:/Users/fede2/OneDrive - University of Miami/MSBA/GasTrackingCLEAN.csv")
 
+# Write the clean dataset out to a csv file on the cloud:
+  write.csv("C:/Users/fede2/OneDrive - University of Miami/MSBA/GasTrackingCLEAN.csv")
